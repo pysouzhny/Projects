0e9c2a8246de1921bd43427398d39dcc18f0b970
pysouzhny <mrngondji@gmail.com>
2016-03-14 01:44:04 -0400

    changes to code and notebook

--- codes/ParseOutText.py
+++ codes/ParseOutText.py
@@ -4,6 +4,22 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from textblob import TextBlob, Word
 from datetime import datetime as dt
 from sklearn.cross_validation import train_test_split
+from sklearn import metrics
+from textblob import TextBlob, Word
+from nltk.stem.snowball import SnowballStemmer
+import pandas as pd 
+import os
+
+
+
+os.chdir(r"C:\Users\Dominique Njinkeu\Documents\GitHub\Projects\notebook")
+df=pd.read_csv("tweets_sample.csv")
+df["Label"]=df.Support.map({"Neutral":0,"Panthers":1,"Broncos":2})
+#determine feature and label variables
+X=df.text
+y=df.Label
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=2,stratify=y)
 
 def split_train_test(X,y):
 	X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=2,stratify=y)
@@ -13,11 +29,11 @@ def split_train_test(X,y):
 
 
 def test_classifier(vect,clf):
-	X_train_dtm=vect.fit_transform(X_train)
-	print "Features: ",X_train_dtm.shape[1]
-	clf.fit(X_train_dtm,y_train)
-	X_test_dtm=vect.transform(X_test)
-    y_pred_class=nb.predict(X_test_dtm)
+    X_train_dtm=vect.fit_transform(X_train)
+    print "Features: ",X_train_dtm.shape[1]
+    clf.fit(X_train_dtm,y_train)
+    X_test_dtm=vect.transform(X_test)
+    y_pred_class=clf.predict(X_test_dtm)
     print "Accuracy: ",metrics.accuracy_score(y_test,y_pred_class)
 
 def confusion_ROC(vect,clf):
@@ -29,6 +45,7 @@ def confusion_ROC(vect,clf):
     print "confusion matrix ",metrics.confusion_matrix(y_test, y_pred_class)
 
 def split_into_lemmas(text):
+    stemmer = SnowballStemmer('english')
     text = unicode(text, 'utf-8').lower()
     words = TextBlob(text).words
     return [word.lemmatize() for word in words]
